988eee0b9f2cdefc304cca4db07b43f075586eb1
magnito2 <magnusotwani@gmail.com>
2019-05-05 12:56:59 +0300

    added frontend build

---
+++ clone-front/build/precache-manifest.7999b052ca14de2dc9eb881ee21af697.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa08733779443e7ca16b",
+    "url": "/static/js/main.78c861e2.chunk.js"
+  },
+  {
+    "revision": "835a5e57b978459a1e07",
+    "url": "/static/js/2.63acd9d7.chunk.js"
+  },
+  {
+    "revision": "fa08733779443e7ca16b",
+    "url": "/static/css/main.8fa00519.chunk.css"
+  },
+  {
+    "revision": "835a5e57b978459a1e07",
+    "url": "/static/css/2.9e8745fe.chunk.css"
+  },
+  {
+    "revision": "da3a730628be49a7ebcb3aac4c6ac6d9",
+    "url": "/index.html"
+  }
+];
