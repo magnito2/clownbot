3dbcf1d94d63c63b00330e990861d40847ca52e0
magnito2 <magnusotwani@gmail.com>
2019-08-24 03:37:48 +0300

    added testing celebro set up script

---
+++ test_up_repl.py
@@ -0,0 +1,30 @@
+#use this to have repl initialize
+#use anaconda ipython
+
+import configparser, time
+import logging
+
+import logging.handlers
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.DEBUG)
+
+logger = logging.getLogger('clone')
+
+fh = logging.handlers.RotatingFileHandler("logs/bot.log", maxBytes=100000, backupCount=5, encoding='utf-8')
+formatter = logging.Formatter('%(asctime)s  - %(name)s - %(levelname)s - %(message)s')
+formatter.converter = time.gmtime
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
+
+from celebro import Celebro
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+logger.setLevel(config.getint('DEFAULT','LOG_LEVEL'))
+celebro_instance = Celebro(config)
